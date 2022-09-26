c74f0fecb0689f23dd719087357191ed498c775a
Alexandr_Borzenkov <forestkom@mail.ru>
2022-09-26 22:16:18 +0700

    2_2

--- homeWork2/Program.cs
+++ homeWork2/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+﻿//Программа которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
     while(true)
     {
         string input = Console.ReadLine();
@@ -7,5 +8,11 @@
         else
             break;
     }
+//Программа которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+    Console.WriteLine("Введите число:");
+    
+int number = Convert.ToInt32(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
 
+Console.WriteLine(numberS.Length > 2 ? $"{number} -> {numberS[2]}" : $"{numberS} -> третьей цифры нет");
     
