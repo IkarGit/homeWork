d5c6e0dffe898b3ba7b2b619fb16c9384d7676e5
Alexandr_Borzenkov <forestkom@mail.ru>
2022-09-26 22:06:01 +0700

    2_1

--- homeWork2/Program.cs
+++ homeWork2/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Введите трехзначное число.\nВыхода из программы команда Exit.");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
+
+    
