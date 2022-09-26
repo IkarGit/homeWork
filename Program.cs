9211a30c61b5eeab7d7e6690979429c85516aeeb
Alexandr_Borzenkov <forestkom@mail.ru>
2022-09-26 20:00:19 +0700

    первое задание

--- Program.cs
+++ Program.cs
@@ -3,6 +3,14 @@ Console.Clear();
 Console.WriteLine("Введите два числа");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("{0} наибольшее число",a);
 
+}
+else
+{
+   Console.WriteLine("{0} наибольшее число",b); 
+}
 
-
+Console.ReadLine();
